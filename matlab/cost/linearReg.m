297b8660d47a54f49a57efd0088969b2a07a7690
huangyi <huangyi@youzan.com>
2019-02-21 21:35:45 +0800

    feat: 线性回归正则化代价函数

---
+++ matlab/cost/linearReg.m
@@ -0,0 +1,17 @@
+function [J, grad] = linearReg(X, y, theta, lambda)
+%linearReg 线性回归正则化代价函数
+
+[m, n] = size(X);
+
+% 模型函数计算结果
+h = X * theta;
+
+% 正则化参数
+lambdaArr = zeros(n, 1) + lambda;
+lambdaArr(1) = 0;
+
+% 计算代价函数
+J = ((h-y)' * (h-y) + lambdaArr' * theta.^2) / (m*2);
+grad = ((h-y)' * X + lambdaArr' .* theta') ./ m;
+grad = grad(:);
+end
