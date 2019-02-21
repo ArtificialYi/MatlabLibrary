35f5b98603591927ab8861f6ef0272b34e2b7c19
huangyi <huangyi@youzan.com>
2019-02-21 18:21:05 +0800

    feat: 多项式扩充特征

---
+++ matlab/lib/mapFeature2polynomial.m
@@ -0,0 +1,43 @@
+function out = mapFeature2polynomial(X, p)
+% mapFeature 多项式扩充特征
+% X 原始数据
+% p 多项式次数
+
+%   获取有效特征
+noneConstX = trimConst(X);
+[m, n] = size(noneConstX);
+
+%   初始化结果矩阵
+tmpN = zeros(1, p - 1);
+for i=2:p
+    tmpN(i - 1) = numOfPolynomialFeature(n, i);
+end
+out = [noneConstX, zeros(m, sum(tmpN))];
+
+%   为结果赋值
+tmpOutCol = n;
+for i=2:p
+    % 获取扩充特征所有排列组合
+    powerMatrix = matrixOfSumWithNum(n, i);
+    featureNum = size(powerMatrix, 1);
+    
+    % 使原始数据和排列组合的行数保持一致
+    repeatNoneConstX = repeatMatrix(noneConstX, featureNum);
+    multiPowerMatrix = multiMatrix(powerMatrix, m);
+    
+    % 开始计算
+    tmpFeatureX = repeatNoneConstX .^ multiPowerMatrix;
+    tmpFeatureVec = prod(tmpFeatureX, 2);
+    
+    % 重新组合成需要的特征矩阵
+    tmpX = reshape(tmpFeatureVec, featureNum, m);
+    
+    % 将特征融合到结果中
+    out(:, (tmpOutCol + 1):(tmpOutCol + featureNum)) = tmpX';
+    tmpOutCol = tmpOutCol + featureNum;
+end
+
+% 去除无用特征
+out = trimConst(out);
+end
+
