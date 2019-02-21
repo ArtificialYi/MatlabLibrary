7eaa9f4d743218c548c9c48a01488bec5be8bcc3
huangyi <huangyi@youzan.com>
2019-02-22 00:03:20 +0800

    feat: 线性回归正则化训练函数

---
+++ matlab/train/linearRegTrain.m
@@ -0,0 +1,25 @@
+function [theta, maxIter, Jval] = linearRegTrain(X, y, lambda, initTheta)
+%linearRegTrain 使用线性回归正则化来训练X
+% X 原始数据
+% y 原始结果
+% lambda 正则化参数
+% initTheta 初始化theta
+
+% 定义代价函数
+costFunction = @(t) linearRegCostFunction(X, y, t, lambda);
+
+% 训练参数
+maxIter = 1024;
+options = optimset('MaxIter', maxIter, 'GradObj', 'on', 'Algorithm', 'trust-region');
+
+% 开始训练
+exitFlag = 0;
+tmpTheta = initTheta;
+while exitFlag == 0
+    [theta, Jval, exitFlag] = ...
+        fminunc(costFunction, tmpTheta, options);
+    tmpTheta = theta;
+    maxIter = maxIter * 2;
+    options = optimset(options, 'MaxIter', maxIter);
+end
+end
