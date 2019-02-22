8d4dee643ea612a93b78d41b694d6a08db0059ac
huangyi <huangyi@youzan.com>
2019-02-23 00:38:43 +0800

    feat: 线性回归训练正则化曲线

---
+++ matlab/linearReg/linearRegTrainForLambda.m
@@ -0,0 +1,15 @@
+function [errorTrain, errorVal] = ...
+    linearRegTrainForLambda(X, y, Xval, yval, initTheta, lambdaVec)
+%linearRegTrainForLambda 查找线性回归在不同的lambda下的结果
+
+% 错误集合
+errorTrain = zeros(length(lambdaVec), 1);
+errorVal = zeros(length(lambdaVec), 1);
+
+for i = 1:length(lambdaVec)
+    thetaTmp = linearRegTrain(X, y, lambdaVec(i), initTheta);
+    errorTrain(i) = linearRegCost(X, y, thetaTmp, 0);
+    errorVal(i) = linearRegCost(Xval, yval, thetaTmp, 0);
+end
+
+end
