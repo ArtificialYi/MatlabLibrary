f5a04e42b6747e36ae9a033d218422ef30af014d
huangyi <huangyi@youzan.com>
2019-02-23 01:02:15 +0800

    fix: 查找线性回归在不同的lambda下的结果-变量名修改

--- matlab/linearReg/linearRegTrainForLambda.m
+++ matlab/linearReg/linearRegTrainForLambda.m
@@ -1,5 +1,5 @@
 function [errorTrain, errorVal] = ...
-    linearRegTrainForLambda(X, y, Xval, yval, initTheta, lambdaVec)
+    linearRegTrainForLambda(X, y, XVal, yVal, initTheta, lambdaVec)
 %linearRegTrainForLambda 查找线性回归在不同的lambda下的结果
 
 % 错误集合
@@ -9,7 +9,7 @@ errorVal = zeros(length(lambdaVec), 1);
 for i = 1:length(lambdaVec)
     thetaTmp = linearRegTrain(X, y, lambdaVec(i), initTheta);
     errorTrain(i) = linearRegCost(X, y, thetaTmp, 0);
-    errorVal(i) = linearRegCost(Xval, yval, thetaTmp, 0);
+    errorVal(i) = linearRegCost(XVal, yVal, thetaTmp, 0);
 end
 
 end
