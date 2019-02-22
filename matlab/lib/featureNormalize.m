de288757bd0b21d2bb45004df4bf4ee21d94bc3e
huangyi <huangyi@youzan.com>
2019-02-22 17:45:13 +0800

    feat: 特征缩放函数

---
+++ matlab/lib/featureNormalize.m
@@ -0,0 +1,16 @@
+function [normNoneConstX, mu, sigma] = featureNormalize(X)
+%featureNormalize 特征缩放
+% X 原始数据集
+
+% 清除常量变量
+noneConstX = trimConst(X);
+
+% 减去平均值
+mu = mean(noneConstX);
+avgNoneConstX = bsxfun(@minus, noneConstX, mu);
+
+% 除去标准差
+sigma = std(avgNoneConstX);
+normNoneConstX = bsxfun(@rdivide, avgNoneConstX, sigma);
+
+end
