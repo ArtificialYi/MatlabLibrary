39c2e8dbe05bea3fd7939578510b0001ea15ee18
huangyi <huangyi@youzan.com>
2019-03-06 04:16:19 +0800

    fix: svm-代价函数-C2lambda

--- matlab/svm/svmCost.m
+++ matlab/svm/svmCost.m
@@ -1,10 +1,10 @@
-function [J] = svmCost(X, Y, w, b, C)
+function [J] = svmCost(X, Y, w, b, lambda)
 %svmCost SVM代价函数
 % X 原始数据
 % Y 原始结果
 % w theta
 % b 截距
-% C 正则化程度
+% lambda 正则化程度
 
 m = size(Y,1);
 Y(Y==0) = -1;
@@ -12,6 +12,7 @@ Y(Y==0) = -1;
 predTheta = (X*w+b).*Y;
 predTheta(predTheta>1)=1;
 
-J = (sum(1-predTheta) + w'*w/(2*C)) / m;
+J = (sum(1-predTheta) + w'*w*lambda/2) / m;
+
 end
 
