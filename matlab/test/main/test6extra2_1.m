21fb9730a577a450666bfa93ca43ba26dedb64a1
huangyi <huangyi@youzan.com>
2019-03-12 04:36:10 +0800

    feat: 服务器matlab代码运行

--- matlab/test/main/test6extra2_1.m
+++ matlab/test/main/test6extra2_1.m
@@ -4,3 +4,7 @@ clear; close all; clc;
 %% 读取原始数据-抽象出测试集、训练集
 % 读取原始数据
 data = load('../resource/ex3data1.mat');
+XOrigin = data.X;
+YOrigin = data.y;
+
+fprintf('X的大小为:%d, %d\n', size(XOrigin, 1), size(XOrigin, 2));
