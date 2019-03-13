4822c5fb88b4057321ed1b609bd681265a43717d
huangyi <huangyi@youzan.com>
2019-03-14 07:49:01 +0800

    feat: 基础训练模型

--- matlab/test/main/test6extra3_multi_0.m
+++ matlab/test/main/test6extra3_multi_0.m
@@ -3,3 +3,67 @@ clear; close all; clc;
 
 %% 读取数据
 % 读取数据
+data = load('resource/ex2data1.txt');
+XOrigin = data(:,1:2);
+YOrigin = data(:,3);
+m = size(XOrigin, 1);
+
+trainPoint = 0.7;
+valPoint = 0.3;
+
+% 切成训练集、交叉验证集、测试集
+indexVecRand = randperm(m);
+[XTrain, YTrain, XVal, YVal, XTest, YTest] = ...
+    splitOriginData(XOrigin, YOrigin, indexVecRand, trainPoint, valPoint);
+
+% 归一化数据
+[XTrainNorm, mu, sigma, noneIndex] = featureNormalize(XTrain);
+XOriginNorm = ...
+    mapFeatureWithParam(XOrigin, 1, noneIndex, 1:length(noneIndex), mu, sigma);
+XValNorm = ...
+    mapFeatureWithParam(XVal, 1, noneIndex, 1:length(noneIndex), mu, sigma);
+
+% 边界线数据准备
+minX1 = min(XOrigin(:,1));
+maxX1 = max(XOrigin(:,1));
+minX2 = min(XOrigin(:,2));
+maxX2 = max(XOrigin(:,2));
+
+splitTrain = 51;
+vecX1 = linspace(minX1, maxX1, splitTrain)';
+vecX2 = linspace(minX2, maxX2, splitTrain)';
+vecX1Repeat = repeatMatrix(vecX1, splitTrain);
+vecX2Multi = multiMatrix(vecX2, splitTrain);
+
+%% 基础训练模型
+CTrain = 4.16;
+tolTrain = 1e-5;
+maxIterTrain = 10;
+alphaTrain = zeros(m, 1);
+
+modelOrigin = ...
+    svmTrain(XOriginNorm, YOrigin, CTrain, alphaTrain, tolTrain, maxIterTrain);
+
+% 训练结果预测
+XTestTmp = [vecX1Repeat vecX2Multi];
+nTestTmp = size(XTestTmp, 2);
+
+XTestTmpNorm = ...
+    mapFeatureWithParam(XTestTmp, 1, noneIndex, noneIndex, mu, sigma);
+
+predYTestTmp = XTestTmpNorm*modelOrigin.w+modelOrigin.b;
+predYTestTmp_2D = reshape(predYTestTmp, splitTrain, splitTrain);
+
+%% 画出数据图
+% 原始数据图
+figure(1);
+posOrigin = find(YOrigin == 1); 
+negOrigin = find(YOrigin == 0);
+
+plot(XOrigin(posOrigin, 1), XOrigin(posOrigin, 2), 'k+','LineWidth', 1, 'MarkerSize', 7);
+hold on;
+plot(XOrigin(negOrigin, 1), XOrigin(negOrigin, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
+contour(vecX1, vecX2, predYTestTmp_2D, [0 0]);
+title('原始数据图');
+fprintf('原始数据图\n');
+hold off;
