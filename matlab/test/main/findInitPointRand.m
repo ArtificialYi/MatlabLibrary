e5dfb451554d4618770646298501986499a419da
huangyi <huangyi@youzan.com>
2019-03-27 17:09:42 +0800

    feat: 测试

--- matlab/test/main/findInitPointRand.m
+++ matlab/test/main/findInitPointRand.m
@@ -1,14 +1,14 @@
-function [centroids] = findInitPointRand(X, K)
+function [centroids] = findInitPointRand(XGPU, KGPU)
 %findPointRand 在X上寻找K个初始点，使用最远距离方案
 
-[m, n] = size(X);
+[m, n] = size(XGPU);
 
-centroids = gpuArray.zeros(K, n);
+centroids = gpuArray.zeros(KGPU, n);
 indexTmp = ceil(rand()*m);
 
-centroids(1, :) = X(indexTmp, :);
-for i=2:K
-    [pointTmp, findSuccess] = findFarPoint(X, centroids(1:i-1, :));
+centroids(1, :) = XGPU(indexTmp, :);
+for i=2:KGPU
+    [pointTmp, findSuccess] = findFarPoint(XGPU, centroids(1:i-1, :));
     if ~findSuccess 
         break;
     end
