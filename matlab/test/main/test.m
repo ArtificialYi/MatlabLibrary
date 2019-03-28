801875200476f131bc24f5b5b97346177ce474f9
huangyi <huangyi@youzan.com>
2019-03-28 20:53:06 +0800

    feat: 画出数据

--- matlab/test/main/test.m
+++ matlab/test/main/test.m
@@ -1,16 +1,2 @@
 %% 初始化环境
 clear; close all; clc;
-
-% 28定律
-scall = 0.2732;
-
-% 基础入参
-xVec = linspace(0,1, 1001);
-xVec = xVec(2:end);
-
-yVec = scall.^(log(xVec)/log(1-scall));
-dVYVec = yVec*log(scall)./(xVec*log(1-scall));
-
-% 画图图形
-
-plot(xVec, yVec, xVec, dVYVec);
---
+++ matlab/test/main/testLogisticReg0.m
@@ -0,0 +1,47 @@
+function [logisticRes] = testLogisticReg0()
+%testLogisticReg0 逻辑回归测试函数
+
+%% 读取数据
+data = load('resource/ex2data1.txt');
+XOrigin = data(:, 1:2);
+YOrigin = data(:, 3);
+
+trainPoint = 0.7;
+valPoint = 0.3;
+
+% 切成训练集、交叉验证集、测试集
+indexVecRand = randperm(m);
+[XTrain, XVal, XTest] = ...
+    splitData(XOrigin, indexVecRand, trainPoint, valPoint);
+[YTrain, YVal, YTest] = ...
+    splitData(YOrigin, indexVecRand, trainPoint, valPoint);
+
+% 归一化数据
+[XTrainNorm, mu, sigma, noneIndex] = featureNormalize(XTrain);
+XOriginNorm = ...
+    mapFeatureWithParam(XOrigin, 1, noneIndex, 1:length(noneIndex), mu, sigma);
+XValNorm = ...
+    mapFeatureWithParam(XVal, 1, noneIndex, 1:length(noneIndex), mu, sigma);
+
+% 边界线数据准备
+minX1 = min(XOrigin(:,1));
+maxX1 = max(XOrigin(:,1));
+minX2 = min(XOrigin(:,2));
+maxX2 = max(XOrigin(:,2));
+
+splitTrain = 101;
+vecX1 = linspace(minX1, maxX1, splitTrain)';
+vecX2 = linspace(minX2, maxX2, splitTrain)';
+vecX1Repeat = repeatMatrix(vecX1, splitTrain);
+vecX2Multi = multiMatrix(vecX2, splitTrain);
+
+%% save
+% 获取文件名
+fileName = sprintf('data/data_testLogisticReg0_%s.mat', datestr(now, 'yyyymmddHHMMss'));
+fprintf('正在保存文件:%s\n', fileName(6:end));
+save(fileName, ...
+    'XOrigin', 'XTrain', 'XVal', 'YOrigin', 'YTrain', 'YTest', ...
+    'vecX1', 'vecX2');
+fprintf('保存完毕\n');
+end
+
