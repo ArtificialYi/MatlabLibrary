54336bbadd5b3936f6340d4919a0e105c5effdfc
huangyi <huangyi@youzan.com>
2019-03-29 20:18:53 +0800

    feat: 逻辑回归代价函数

--- matlab/test/main/logisticRegCostFunc.m
+++ matlab/test/main/logisticRegCostFunc.m
@@ -1,11 +1,10 @@
-function [J, grad] = logisticCostFunc(X, Y, theta)
+function [J, grad] = logisticRegCostFunc(X, Y, theta)
 %logisticCostFunc 逻辑回归的代价函数
 
 m = size(X, 1);
 
 h = logisticHypothesis(X, theta);
 J = sum(-Y.*log(h) - (1-Y).*log(1-h))/m;
-grad = sum((h-Y).*X)/m;
-
+grad = (h-Y)'*X/m;
 end
 
