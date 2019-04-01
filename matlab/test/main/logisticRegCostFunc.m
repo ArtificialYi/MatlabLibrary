fa260950f07af84645a55d3e668c2dbd21f254b2
huangyi <huangyi@youzan.com>
2019-04-01 19:09:25 +0800

    feat: 修改代价函数

--- matlab/test/main/logisticRegCostFunc.m
+++ matlab/test/main/logisticRegCostFunc.m
@@ -2,10 +2,11 @@ function [J, grad] = logisticRegCostFunc(X, Y, theta)
 %logisticCostFunc 逻辑回归的代价函数
 
 m = size(X, 1);
+pred = 1e-100;
 
 h = logisticHypothesis(X, theta);
 
 J = (-Y'*log(h) - ( 1-Y')*log(1-h))/m;
-grad = X'*(h-Y)/m;
+grad = (X'*(h-Y)/m)*(1-2*pred);
 end
 
