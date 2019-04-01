1141f72e5c56d78847f2aa5ddc5a5ba26e6e3b12
huangyi <huangyi@youzan.com>
2019-04-01 19:08:00 +0800

    feat: 修改假设函数

--- matlab/test/main/logisticHypothesis.m
+++ matlab/test/main/logisticHypothesis.m
@@ -1,12 +1,9 @@
-function [h] = logisticHypothesis(X, theta)
+function [h] = logisticHypothesis(X, theta, pred)
 %logisticHypothesis 逻辑回归假设函数
 
 hUnit = 1 ./ (exp(-X*theta)+1);
 
-% 计算精度
-pred = 1e-100;
-
-h = (1-2*pred)*(2*hUnit-1)/2;
+h = hUnit - 2*pred*hUnit + pred;
 
 end
 
--- matlab/test/main/logisticRegCostFunc.m
+++ matlab/test/main/logisticRegCostFunc.m
@@ -2,9 +2,10 @@ function [J, grad] = logisticRegCostFunc(X, Y, theta)
 %logisticCostFunc 逻辑回归的代价函数
 
 m = size(X, 1);
-h = logisticHypothesis(X, theta);
-J = (-Y'*log(h) - (1-Y')*log((1-h)))/m;
-grad = X'*(h-Y)/m;
 
+h = logisticHypothesis(X, theta);
+
+J = (-Y'*log(h) - ( 1-Y')*log(1-h))/m;
+grad = X'*(h-Y)/m;
 end
 
