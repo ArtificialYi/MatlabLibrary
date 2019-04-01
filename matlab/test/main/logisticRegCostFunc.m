c30d718a6d412c3d144f6010153cfd146a16b32c
huangyi <huangyi@youzan.com>
2019-04-01 18:47:26 +0800

    feat: 添加精度计算

--- matlab/test/main/logisticRegCostFunc.m
+++ matlab/test/main/logisticRegCostFunc.m
@@ -1,9 +1,12 @@
 function [J, grad] = logisticRegCostFunc(X, Y, theta)
 %logisticCostFunc 逻辑回归的代价函数
 
+% log计算精度
+pred = 1e-100;
+
 m = size(X, 1);
 h = logisticHypothesis(X, theta);
-J = (-Y'*log(h) - (1-Y')*log(1-h))/m;
+J = (-Y'*log(h+pred) - (1-Y')*log((1-h+pred)))/m;
 grad = X'*(h-Y)/m;
 
 if isnan(J)
