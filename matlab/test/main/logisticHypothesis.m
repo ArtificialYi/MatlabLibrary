c78a8e5c4d35974714ec7ae34293eb5b3f0714be
huangyi <huangyi@youzan.com>
2019-04-01 19:00:00 +0800

    feat: 计算精度

--- matlab/test/main/logisticHypothesis.m
+++ matlab/test/main/logisticHypothesis.m
@@ -1,7 +1,12 @@
 function [h] = logisticHypothesis(X, theta)
 %logisticHypothesis 逻辑回归假设函数
 
-h = 1 ./ (exp(-X*theta)+1);
+hUnit = 1 ./ (exp(-X*theta)+1);
+
+% 计算精度
+pred = 1e-100;
+
+h = (1-2*pred)*(2*hUnit-1)/2;
 
 end
 
--- matlab/test/main/logisticRegCostFunc.m
+++ matlab/test/main/logisticRegCostFunc.m
@@ -1,29 +1,10 @@
 function [J, grad] = logisticRegCostFunc(X, Y, theta)
 %logisticCostFunc 逻辑回归的代价函数
 
-% log计算精度
-pred = 1e-100;
-
 m = size(X, 1);
 h = logisticHypothesis(X, theta);
-J = (-Y'*log(h+pred) - (1-Y')*log((1-h+pred)))/m;
+J = (-Y'*log(h) - (1-Y')*log((1-h)))/m;
 grad = X'*(h-Y)/m;
-
-if isnan(J)
-    disp(X);
-    disp(Y);
-    disp(theta);
-    disp(h);
-    
-    showHy(X, 'X');
-    showHy(Y, 'Y');
-    showHy(theta, 'theta');
-    showHy(m, 'm');
-    showHy(h, 'h');
-    showHy(J, 'J');
-    showHy(grad, 'grad');
-    pause;
-end
 
 end
 
