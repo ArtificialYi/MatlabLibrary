924615765a49306f25ea65c08d5e5293f7c13e86
huangyi <huangyi@youzan.com>
2019-04-04 03:59:42 +0800

    feat: 移动画图函数位置

--- matlab/plot/plotContourY.m
+++ matlab/plot/plotContourY.m
@@ -10,7 +10,14 @@ for i=1:3
         case 2
             c = contour(XMatrix(:, 1), XMatrix(:, 2), YMatrix, [high high]);
         otherwise
-            c = contour3(XMatrix(:, 1), XMatrix(:, 2), XMatrix(:, 3), YMatrix, [high high]);
+            for j=1:length(XMatrix(:, 3))
+                matrixTmp0 = YMatrix(:, :, j);
+                matrixTmp1 = matrixTmp0;
+                matrixTmp1(matrixTmp0<high) = XMatrix(j, 3) - 1;
+                matrixTmp1(matrixTmp0==high) = XMatrix(j, 3);
+                matrixTmp1(matrixTmp0>high) = XMatrix(j, 3) + 1;
+                c = contour3(XMatrix(:, 1), XMatrix(:, 2), matrixTmp1, [XMatrix(j, 3) XMatrix(j, 3)]);
+            end
     end
 end
 
