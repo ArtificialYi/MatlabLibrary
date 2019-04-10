0ef81f6cac5c22d12c0040e800065f53698c6318
huangyi <huangyi@youzan.com>
2019-04-11 02:52:36 +0800

    feat: 修改高斯核名称

--- matlab/svm/kernel/svmKernelGaussian.m
+++ matlab/svm/kernel/svmKernelGaussian.m
@@ -1,14 +1,12 @@
-function [K, K2] = svmKernelGaussian(X1, X2, gu)
+function [K] = svmKernelGaussian(X1, X2, gu)
 %svmKernelGaussian 高斯核函数
 
 % 取出核数据
-m1 = size(X1, 1);
 m2 = size(X2, 1);
 
 K = X2(:, 1) + X1(:, 1)';
-K2 = K;
 for i=1:m2
-    K2(i, :) = exp(-sum((X2(i, :) - X1).^2,2)./(2*gu^2));  
+    K(i, :) = exp(-sum((X2(i, :) - X1).^2,2)./(2*gu^2));  
 end
 
 end
