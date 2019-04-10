6504a0c7829f76a951b990461ad577925ac6ecf4
huangyi <huangyi@youzan.com>
2019-04-10 15:58:49 +0800

    feat: 查看pca数据

---
+++ matlab/test/main/testComp.m
@@ -0,0 +1,90 @@
+function [outputArg1,outputArg2] = testComp()
+%testComp 比赛用的函数
+
+%% 先读取数据
+data = load('resource/pfm_data.mat');
+
+% 获取原始数据
+XOrigin = data.XOrigin;
+YOrigin = data.YOrigin;
+XTest = data.XTest;
+
+[mOrigin, nOrigin] = size(XOrigin);
+
+trainPoint = 0.7;
+valPoint = 0.3;
+pred = 1e-16;
+
+% 切成训练集、交叉验证集、测试集
+indexVecRand = randperm(mOrigin);
+[XTrain, XVal, ~] = ...
+    splitData(XOrigin, indexVecRand, trainPoint, valPoint);
+[YTrain, YVal, ~] = ...
+    splitData(YOrigin, indexVecRand, trainPoint, valPoint);
+
+% 获取基础数据
+mTrain = size(XTrain, 1);
+mVal = size(XVal, 1);
+mTest = size(XTest, 1);
+
+% 归一化数据
+[XTrainNorm, data2normFunc] = data2featureWithNormalize(XTrain, p);
+nTrain = size(XTrainNorm, 2);
+XOriginNorm = data2normFunc(XOrigin);
+XValNorm = data2normFunc(XVal);
+XTestNorm = data2normFunc(XTest);
+
+%% GPU数据
+% 归一化数据
+XOriginNormGPU = gpuArray(XOriginNorm);
+XTrainNormGPU = gpuArray(XTrainNorm);
+XValNormGPU = gpuArray(XValNorm);
+XTestNormGPU = gpuArray(XTestNorm);
+nTrainGPU = gpuArray(nTrain);
+predGPU = gpuArray(pred);
+
+% 真实数据
+XOriginNormRealGPU = [ones(m, 1) XOriginNormGPU];
+XTrainNormRealGPU = [ones(mTrain, 1) XTrainNormGPU];
+XValNormRealGPU = [ones(mVal, 1) XValNormGPU];
+XTestNormRealGPU = [ones(mTest, 1) XTestNormGPU];
+
+% 结果数据
+YOriginGPU = gpuArray(YOrigin);
+YTrainGPU = gpuArray(YTrain);
+YValGPU = gpuArray(YVal);
+
+% 默认参数
+thetaInitGPU = gpuArray.zeros(nTrain+1, 1);
+lambdaGPU = gpuArray(lambda);
+maxIterGPU = gpuArray(maxIter);
+
+% 学习曲线
+splitLearningCurve = 50;
+splitLearningCurveGPU = gpuArray(splitLearningCurve);
+
+%% pca提取
+[UTrainGPU, STrainGPU] = pcaTrainGPU(XTrainNormGPU);
+
+% pcaVec
+pcaVecGPU = diag(STrainGPU);
+pcaSumVecGPU = pcaVecGPU;
+for i=2:length(pcaSumVecGPU)
+    pcaSumVecGPU(i) = pcaSumVecGPU(i-1)+pcaVecGPU(i);
+end
+
+pcaVec = gather(pcaVecGPU);
+pcaSumVec = gather(pcaSumVecGPU);
+
+%% save
+% 获取文件名
+fileName = sprintf('data/data_testComp_%s.mat', datestr(now, 'yyyymmddHHMMss'));
+fprintf('正在保存文件:%s\n', fileName(6:end));
+save(fileName, ...
+    'XOrigin', 'XTrain', 'XVal', 'XTest', ...
+    'YOrigin', 'YTrain', 'YVal', 'YTest', ...
+    'XOriginNormPca', 'XTrainNormPca', 'XValNormPca', 'XTestNormPca', ...
+    'pcaVec', 'pcaSumVec');
+fprintf('保存完毕\n');
+end
+
