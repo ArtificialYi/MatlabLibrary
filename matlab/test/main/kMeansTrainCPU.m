9ad7c37abf74e80716e11bd5d17468300fc465f4
huangyi <huangyi@youzan.com>
2019-05-07 14:43:35 +0800

    feat: kMeansCPU

--- matlab/test/main/kMeansTrainCPU.m
+++ matlab/test/main/kMeansTrainCPU.m
@@ -1,7 +1,35 @@
-function [outputArg1,outputArg2] = kMeansTrainCPU(inputArg1,inputArg2)
-%UNTITLED2 此处显示有关此函数的摘要
-%   此处显示详细说明
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [centroids, idxPtr, distUnit, iter] = kMeansTrainCPU(X, centroids, maxIter)
+%kMeansTrainCPU kMeans训练函数-CPU版
+
+[m, n] = size(X);
+K = size(centroids, 1);
+centroidsRepeat = zeros(m*K, n);
+matrixTmp = zeros(K, m);
+idxPre = zeros(m, 1);
+
+XMulti = multiMatrix(X, K);
+
+for i=1:maxIter
+    % 划分数据点到聚类点
+    centroidsRepeat(:) = repeatMatrix(centroids, m);
+    idxMatrixTmp = reshape(sum((XMulti - centroidsRepeat).^2, 2), m, K);
+    
+    [dist, idxPtr] = min(idxMatrixTmp, [], 2);
+    
+    % 如果没有误差变动
+    if all(idxPre==idxPtr)
+        break;
+    end
+    
+    % 求新的聚类点
+    matrixTmp(:) = 0;
+    matrixTmp((0:m-1)'*K+idxPtr) = 1;
+    centroids(:) = matrixTmp./sum(matrixTmp, 2)*X;
+    
+    idxPre(:) = idxPtr;
 end
 
+iter = i;
+distUnit = mean(dist);
+
+end
