3b5b8047554020dfec84f3d22d63aeb373cbb95b
huangyi <huangyi@youzan.com>
2019-05-14 16:19:16 +0800

    feat: 测试

--- matlab/test/main/svmTrainCPUForCVec.m
+++ matlab/test/main/svmTrainCPUForCVec.m
@@ -6,6 +6,7 @@ errorTrainVec = CVec;
 
 for i=1:length(CVec)
     [errorVal, errorTrain] = func(CVec(i));
+    fprintf('当前计算C:%f,误差:%f\n', CVec(i), errorVal);
     errorValVec(i) = errorVal;
     errorTrainVec(i) = errorTrain;
 end
